4dcbbd8011c3a3978b46a83ee6a8c49d868c4c3d
manuel loutoby <manuel.loutoby@gmail.com>
2024-07-03 16:58:34 +0200

    yelp done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+
+Restaurant.create(name: "Chez Manu", address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number_with_country_code, category: Restaurant::CATEGORIES.sample)
+Restaurant.create(name: "Chez Pierre", address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number_with_country_code, category: Restaurant::CATEGORIES.sample)
+Restaurant.create(name: "Chez Amael", address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number_with_country_code, category: Restaurant::CATEGORIES.sample)
+Restaurant.create(name: "Chez Fredo", address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number_with_country_code, category: Restaurant::CATEGORIES.sample)
+Restaurant.create(name: "Chez Qassim", address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number_with_country_code, category: Restaurant::CATEGORIES.sample)
+
+[Chez_Manu, Chez_Pierre, Chez_Amael, Chez_Fredo, Chez_Qassim].each do |attributes|
+  Restaurant.create!(attributes)
+end
